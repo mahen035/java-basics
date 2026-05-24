6be2a05761a4aa7059f5eff78ac035c86f208bf4
mahen112 <mahen112@mail.com>
2026-05-24 11:27:02 +0530

    User file updated

--- src/com/training/User.java
+++ src/com/training/User.java
@@ -1,7 +1,9 @@
 package com.training;
 
+import java.util.Objects;
+
 //Encapsulation
-public class User{
+public class User implements Comparable<User>{
 	
 	// 1. attributes
 	// instance variables
@@ -44,6 +46,31 @@ public class User{
 	public String toString() {
 		return "User [userId=" + userId + ", userName=" + userName + ", userEmail=" + userEmail + "]";
 	}
+
+	@Override
+	public int hashCode() {
+		
+		return Objects.hash(userEmail, userId, userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(userEmail, other.userEmail) && Objects.equals(userId, other.userId)
+				&& Objects.equals(userName, other.userName);
+	}
+
+	@Override
+	public int compareTo(User o) {
+		return this.userId.compareTo(o.userId);
+	}
+	
 	
 	
 	
